e44960b6bb5e987344700638a02c0c712a27ce16
Rinky9730 <rao678734@gmail.com>
2026-08-19 16:14:57 +0530

    Updated The Base URL

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -4,7 +4,7 @@ import axios from "axios";
 import qs from "qs";
 // console.log("ENV:", import.meta.env.VITE_API_URL);
 const api = axios.create({
-  baseURL: "http://localhost:4000/api",
+  baseURL: "https://fooddelivery-m82b.onrender.com/api",
   withCredentials: true,
   paramsSerializer: (params) => qs.stringify(params, { arrayFormat: "repeat" }),
 });
